6749b653bf2ddbadad4a73d52181d7be07e7804f
Mahmoud Khudairi <mahmoudkhudairi@gmail.com>
2024-01-05 11:52:54 +0300

    done

--- index.js
+++ index.js
@@ -7,8 +7,12 @@ const { Pool } = require('pg');
 
 const pool = new Pool({ connectionString: process.env.ELEPHANT_SQL_CONNECTION_STRING });
 app.use(express.json());
-app.use(cors())
-
+app.use(cors());
+//global middleware
+app.use((req, res, next) => {
+  console.log(req.method, req.path);
+  next();
+});
 app.get('/', (req, res) => {
   res.send('hello world');
 });
@@ -34,7 +38,22 @@ app.get('/api/books/:id', (req, res) => {
     })
     .catch(e => res.status(500).json({ message: e.message }));
 });
-app.post('/api/books', (req, res) => {
+const validateBook = (req, res, next) => {
+  const requiredFields = ['title', 'author', 'year'];
+  let missingFields = '';
+  for (const field of requiredFields) {
+    if (!req.body[field]) {
+      missingFields += `${field} required `;
+    }
+  }
+  if (missingFields) {
+    //return  res.json({error:missingFields})
+    next(missingFields);
+  } else {
+    next();
+  }
+};
+app.post('/api/books', validateBook, (req, res, next) => {
   const { title, author, year } = req.body;
   pool
     .query('INSERT INTO books (title,author,year) VALUES ($1,$2,$3) RETURNING *;', [
@@ -74,6 +93,10 @@ app.delete('/api/books/:id', (req, res) => {
       }
     })
     .catch(e => res.status(500).json({ message: e.message }));
+});
+
+app.use((error, req, res, next) => {
+  res.status(400).json({ error });
 });
 app.listen(PORT, () => {
   console.log('server is running!!');
